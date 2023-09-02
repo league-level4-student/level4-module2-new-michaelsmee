a1159ea287c8ef9dbbffb57a4d8283b4f4fde97b
league <jacktu2718@gmail.com>
2023-09-02 14:25:44 -0700

    Working on adding the images for console store

--- src/_06_Console_Store/ConsoleStore.java
+++ src/_06_Console_Store/ConsoleStore.java
@@ -1,5 +1,11 @@
 package _06_Console_Store;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class ConsoleStore {
 
     /*
@@ -35,9 +41,127 @@ public class ConsoleStore {
      * print out a receipt showing their name, the individual prices of the
      * items and their total.
      */
-
+	static int total;
     public static void main(String[] args) {
+    	int budget = 50;
+    	int aCost = 4;
+    	int oCost = 5;
+    	int cCost = 6;
+    	int mCost = 7;
+    	total = 0;
+    	
+    	ArrayList<String>fruitList = new ArrayList<String>();
+    	ArrayList<Integer>fruitCost = new ArrayList<Integer>();
+    	
+    	
+    	Scanner scan = new Scanner(System.in);
+    	System.out.println("name?");
+    	String name = scan.next();
+    	System.out.println("Do you want to add, remove, or checkout");
+    	String again = scan.next();
+    	 do {
+    		
+        	 if(again.equals("add")) {
+        		 System.out.println("Do you want to buy an apple, orange, cherry, or mango");
+        		 String choice = scan.next();
+        		 
+        		if(choice.equals("apple")) {
+        			fruitList.add("apple");
+        			fruitCost.add(aCost);
+        			total += aCost;
+        		}
+        		else if(choice.equals("orange")) {
+        			fruitList.add("ornage");
+        			fruitCost.add(oCost);
+        			total += oCost;
+        		}
+        		else if(choice.equals("cherry")) {
+        			fruitList.add("cherry");
+        			fruitCost.add(cCost);
+        			total += cCost;
+        		}
+        		else if(choice.equals("mango")) {
+        			fruitList.add("mango");
+        			fruitCost.add(mCost);
+        			total += mCost;
+        		}
+        		else {
+        			System.out.println("error");
+        		}
+        			
+        	 }
+        	 else if(again.equals("remove")) {
+        		 removeMethod(again, fruitCost, fruitList, total, aCost, oCost, cCost, mCost, scan);
+        		 
+        	 }
+    		 
+        	 System.out.println("Do you want to add, remove, or checkout");
+             again = scan.next();
+        } while (!again.equals("checkout")); 
+           
+        while(total > budget) {
+        	System.out.println("you have too much");
+        	removeMethod(again, fruitCost, fruitList, total, aCost, oCost, cCost, mCost, scan);
+        }
+        
 
+        scan.close();
+        System.out.println("Name: " + name);
+        for(int i = 0; i<fruitList.size(); i++) {
+        	System.out.println("Item: " + fruitList.get(i) + " Cost: " + fruitCost.get(i));
+        }
+        System.out.println("Cost: " + total);
+    }
+    
+    
+    public static void removeMethod(String again, ArrayList<Integer>fruitCost, ArrayList<String>fruitList, int total, int aCost, int oCost, int cCost, int mCost, Scanner scan) {
+    	for(int i = 0; i< fruitCost.size(); i++) {
+			 System.out.println("Item: " + fruitList.get(i) + " Cost: " + fruitCost.get(i));
+			 
+		 }
+		 System.out.println("Total spent: " + total);
+		 System.out.println("what index do you want to remove");
+		 int removeIndex = scan.nextInt();
+		 if(fruitList.get(removeIndex).equals("apple")) {
+			total-=aCost; 
+		 }
+		 else if(fruitList.get(removeIndex).equals("orange")) {
+			total-=oCost; 
+		 }
+		 else if(fruitList.get(removeIndex).equals("cherry")) {
+			total-=cCost; 
+		 }
+		 else if(fruitList.get(removeIndex).equals("mango")) {
+			total-=mCost; 
+		 }
+		 System.out.println(total);
+		 ConsoleStore.total = total;
+		 fruitList.remove(removeIndex);
+		 fruitCost.remove(removeIndex);
     }
 
+    
+    public void showCart(ArrayList<String>fruitList) {
+    	JFrame frame = new JFrame();
+    	JPanel panel = new JPanel();
+    	frame.add(panel);
+    	frame.setVisible(true);
+    	for(int i = 0; i < fruitList.size(); i++) {
+    		if(fruitList.get(i).equals("apple")) {
+    			panel.add();
+    		}
+    		else if (fruitList.get(i).equals("orange")) {
+    			panel.add();
+    		}
+    		else if (fruitList.get(i).equals("cherry")) {
+    			panel.add();
+    		}
+    		else if(fruitList.get(i).equals("mango")){
+    			panel.add();
+    		}
+    	}
+    }
+    
+    
+    
 }
